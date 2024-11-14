cff9a1ad54264c9deecd684bdbf6fc897c7aba57
manpreetkaur <Kaur4285@sheridancollege.ca>
2024-11-14 12:19:08 -0500

    Initial Commit

---
+++ ManpreetWeather/ManpreetWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManpreetWeatherApp.swift
+//  ManpreetWeather
+//
+//  Created by manpreet kaur on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct ManpreetWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
